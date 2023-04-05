07dbd8de3e4a6e1cd2244a8d1f5d179c321de748
islamshu12 <islamshu12@gmail.com>
2023-04-06 01:30:49 +0200

    edit

--- resources/views/frontend/_featurs.blade.php
+++ resources/views/frontend/_featurs.blade.php
@@ -5,7 +5,7 @@
 
             @foreach (App\Models\Fteure::get() as $item)
                 
-            <div class="col-xl-3 col-md-6 d-flex" data-aos="zoom-out">
+            <div class="col-xl-3 col-md-6  d-flex justify-content-center" data-aos="zoom-out">
                 <div class="service-item position-relative">
                     <div class="icon"><i class="{{ $item->image }}"></i></div>
                     <h4><a  class="stretched-link">{{ $item->title }}</a></h4>
