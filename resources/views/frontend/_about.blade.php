abd80d9a19b4281b183ca892fbe5324a39212946
islamshu12 <islamshu12@gmail.com>
2023-05-30 01:00:14 +0300

    edit

--- resources/views/frontend/_about.blade.php
+++ resources/views/frontend/_about.blade.php
@@ -10,12 +10,12 @@
         </div>
         <div class="row g-5">
 
-            <div class="col-lg-8 col-md-6 content d-flex flex-column justify-content-center order-last order-md-first">
+            <div class="col-lg-4 col-md-4 content d-flex flex-column justify-content-center order-last order-md-first">
                 <h3>{{ $about->title }}</h3>
                 <p> {!! $about->body !!}</p>
             </div>
 
-            <div class="col-lg-4 col-md-6 order-first order-md-last d-flex align-items-center">
+            <div class="col-lg-8 col-md-8 order-first order-md-last d-flex align-items-center">
                 <div class="img">
                     <img src="{{ asset('uploads/' . $about->image) }}" alt="" class="img-fluid">
                 </div>
